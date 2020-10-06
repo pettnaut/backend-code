e4916a053fc782a13cdf074b4d4758ac9aec2c0e
chjuca <chjuca@utpl.edu.ec>
2020-10-06 18:57:49 -0500

    Add environment variables

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
 
-const database = 'postgres';        // nombre de tu base de datos
-const username = 'postgres';        // nombre de tu usuario
-const password = 'postgres';        // contraseña de el usuario (en este caso el usuario es 'postgres')
+const database = process.env.DATABASE || 'postgres';        // nombre de tu base de datos
+const username = process.env.USER || 'postgres';        // nombre de tu usuario
+const password = process.env.PASSWORD || 'postgres';        // contraseña de el usuario (en este caso el usuario es 'postgres')
 
 export const sequelize = new Sequelize(database, username, password, {
-    host: 'db',
+    host: 'localhost',
     dialect: 'postgres',            // base de datos que usaremos
     pool: {
         max: 5,
