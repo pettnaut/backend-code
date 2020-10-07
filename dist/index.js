82af7f85b4d3ce0fbffa0642779b134e41b8f0d2
chjuca <chjuca@utpl.edu.ec>
2020-10-06 19:31:35 -0500

    fix the $PORT bug in the index dist

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 var _app = _interopRequireDefault(require("./app"));
 
 function main() {
-  _app["default"].listen(4000);
+  _app["default"].listen(process.env.PORT || 4000);
 
   console.log("Server on port 4000");
 }
